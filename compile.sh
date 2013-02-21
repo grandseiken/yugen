9d6da9f51b4da91a422983337ccc8188042ff1bc
stu <github@seiken.co.uk>
2013-02-21 15:59:25 +0000

    Added SFML.

--- compile.sh
+++ compile.sh
@@ -3,6 +3,10 @@ if [ ! -d bin ]
 then
   mkdir bin
 fi
+cflag="-O3 -Wall -std=c++0x -I./depend/boost_1_53_0/include -I./depend/sfml_2_0/include"
+lflag="-O3 -Wall -static -L./depend/boost_1_53_0/lib -L./depend/sfml_2_0/lib"
+libs="-lboost_filesystem -lboost_system -lsfml-system-s -lsfml-window-s"
+
 change=0
 error=0
 for cpp in source/*.cpp; do
@@ -11,7 +15,7 @@ for cpp in source/*.cpp; do
     echo "Up to date: $cpp"
   else
     echo "Compiling $cpp..."
-    g++-4.7 -c -O3 -Wall -std=c++0x -I./boost_1_53_0/install/include -o bin/$base.o $cpp
+    g++-4.7 -c $cflag -o bin/$base.o $cpp
     if [ $? -eq 0 ]; then
       md5sum $cpp > bin/$base.md5
     else
@@ -22,9 +26,14 @@ for cpp in source/*.cpp; do
   fi
 done
 if [ $error -eq 0 ]; then
-  if [ $change -eq 1 ]; then
+  if [ $change -eq 1 ] || [ ! -f bin/yugen.md5 ]; then
     echo "Linking..."
-    g++-4.7 -O3 -Wall -L./boost_1_53_0/install/lib -o bin/yugen bin/*.o -static -lboost_filesystem -lboost_system
+    g++-4.7 $lflag -o bin/yugen bin/*.o $libs
+    if [ $? -eq 0 ]; then
+      md5sum bin/yugen > bin/yugen.md5
+    else
+      rm bin/yugen.md5
+    fi
   else
     echo "Up to date: yugen"
   fi
