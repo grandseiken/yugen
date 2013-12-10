342174a577c59f76759b7e3757ba5d35314e55c2
stu <github@seiken.co.uk>
2013-12-10 23:43:47 +0000

    clarify gamma comments

--- data/shaders/gamma.glsl
+++ data/shaders/gamma.glsl
@@ -1,16 +1,15 @@
-// Textures are stored in sRGB, which *appears* linear (i.e. 0.5 grey appears to
-// be half-way in between black and white). But in fact the human eye is
-// nonlinear, and these values are really "too dark". We need to convert
-// textures (or any input colours which are intended to appear linearly) to
-// a true linear space (which *appears* too bright to the human eye) before
-// performing lighting calculations, and then reverse this again before display.
+// Textures and monitors work with sRGB, which *appears* linear to the human eye
+// (i.e., 0.5 grey seems to be half-way in between black and white). But in fact
+// the eye is nonlinear, and these values are somehow too dark. We need to
+// convert colours to the true linear space (which appears "too bright" to the
+// eye) before performing lighting calculations, and reverse this again before
+// display.
 //
 // On the other hand, blending *colours* should really be done in the nonlinear
-// output space, since for examplei, a blend value of 0.5 really means we want
+// sRGB space, since for example, a blend value of 0.5 really means we want
 // an output half-way between the two colours we would otherwise see.
 // TODO: the gamma correction, right now, is happening in the wrong place (input
-// of textures and final output only) w.r.t. paragraph 2 and the "other input
-// colours" section of paragraph 1.
+// of textures and final output only).
 const bool use_gamma_correction = false;
 const float gamma_value = 2.2;
 const float gamma_div = 1.0 / gamma_value;
