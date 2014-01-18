b0385025ffc46c7936eb78e02d4be78bc4d89624
stu <github@seiken.co.uk>
2014-01-18 20:53:24 +0000

    add type computation. it doesn't seem to work so far

--- src/yang/type_info.h
+++ src/yang/type_info.h
@@ -6,10 +6,15 @@
 #include "../vector.h"
 #include "type.h"
 
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/insert_range.hpp>
+
 namespace yang {
 
 typedef y::int32 int32;
 typedef y::world world;
+template<typename T, y::size N, typename y::enable_if<(N > 1), bool>::type = 0>
+using vec = y::vec<T, N>;
 template<y::size N, typename y::enable_if<(N > 1), bool>::type = 0>
 using int32_vec = y::vec<int32, N>;
 template<y::size N, typename y::enable_if<(N > 1), bool>::type = 0>
@@ -81,21 +86,91 @@ template<typename R, typename A, typename... Args>
 y::function<R(Args...)> bind_first(
     const y::function<R(A, Args...)>& function, const A& arg)
 {
-  return [=](Args... args)
+  return [&](const Args&... args)
   {
     return function(arg, args...);
   };
 }
 
-// Function call instrumentation for converting native types to the Yang calling
-// convention for trampoline functions. TrampolineArgs unpacks the argument
-// list; TrampolineCall unpacks the return value.
+// Templates for converting native function types into the corresponding
+// Yang trampoline function types.
+template<typename... Args>
+using List = boost::mpl::list<Args...>;
+
+template<typename... Args>
+struct Functions {
+  typedef void (*fp_type)(Args...);
+  typedef y::function<void(Args...)> f_type;
+};
+template<typename T>
+struct FunctionsFromList {};
+template<typename... Args>
+struct FunctionsFromList<List<Args...>> {
+  typedef typename Functions<Args...>::fp_type fp_type;
+  typedef typename Functions<Args...>::f_type f_type;
+};
+
+template<typename R>
+struct TrampolineReturn {
+  typedef List<R*> type;
+};
+template<>
+struct TrampolineReturn<void> {
+  typedef List<> type;
+};
+template<typename T>
+struct TrampolineReturn<vec<T, 2>> {
+  typedef List<T*, T*> type;
+};
+template <typename T, y::size N>
+struct TrampolineReturn<vec<T, N>> {
+  typedef typename boost::mpl::push_front<
+      typename TrampolineReturn<vec<T, N - 1>>::type, T*>::type type;
+};
+
 template<typename... Args>
 struct TrampolineArgs {};
-
-// TrampolineArgs base case.
+template<typename A, typename... Args>
+struct TrampolineArgs<A, Args...> {
+  typedef typename boost::mpl::push_front<
+      typename TrampolineArgs<Args...>::type, A>::type type;
+};
 template<>
 struct TrampolineArgs<> {
+  typedef List<> type;
+};
+template<typename T, typename... Args>
+struct TrampolineArgs<vec<T, 2>, Args...> {
+  typedef typename TrampolineArgs<Args...>::type rest;
+  typedef typename boost::mpl::push_front<
+      typename boost::mpl::push_front<rest, T>::type, T>::type type;
+};
+template<typename T, y::size N, typename... Args>
+struct TrampolineArgs<vec<T, N>, Args...> {
+  typedef typename TrampolineArgs<vec<T, N - 1>, Args...>::type rest;
+  typedef typename boost::mpl::push_front<rest, T>::type type;
+};
+
+template<typename R, typename... Args>
+struct TrampolineType {
+  typedef typename TrampolineReturn<R>::type ret_type;
+  typedef typename TrampolineArgs<Args...>::type args_type;
+  typedef typename boost::mpl::insert_range<
+      args_type,
+      typename boost::mpl::begin<args_type>::type, ret_type>::type cat_type;
+  typedef typename FunctionsFromList<cat_type>::fp_type fp_type;
+  typedef typename FunctionsFromList<cat_type>::f_type f_type;
+};
+
+// Function call instrumentation for converting native types to the Yang calling
+// convention for trampoline functions. TrampolineCallArgs unpacks the argument
+// list; TrampolineCall unpacks the return value.
+template<typename... Args>
+struct TrampolineCallArgs {};
+
+// TrampolineCallArgs base case.
+template<>
+struct TrampolineCallArgs<> {
   typedef y::function<void()> f_type;
 
   void operator()(const f_type& function) const
@@ -104,14 +179,14 @@ struct TrampolineArgs<> {
   }
 };
 
-// TrampolineArgs unpacking of a single primitive.
+// TrampolineCallArgs unpacking of a single primitive.
 template<typename A, typename... Args>
-struct TrampolineArgs< A, Args...> {
+struct TrampolineCallArgs< A, Args...> {
   typedef y::function<void(A, Args...)> f_type;
 
   void operator()(const f_type& function, const A& arg, const Args&... args) const
   {
-    typedef TrampolineArgs<Args...> next_type;
+    typedef TrampolineCallArgs<Args...> next_type;
     next_type()(bind_first(function, arg), args...);
   }
 };
@@ -125,7 +200,7 @@ struct TrampolineCall {
   R operator()(const f_type& function, const Args&... args) const
   {
     R result;
-    typedef TrampolineArgs<R*, Args...> args_type;
+    typedef TrampolineCallArgs<R*, Args...> args_type;
     args_type()(function, &result, args...);
     return result;
   }
@@ -139,7 +214,7 @@ struct TrampolineCall<void, Args...> {
 
   void operator()(const f_type& function, const Args&... args) const
   {
-    typedef TrampolineArgs<Args...> args_type;
+    typedef TrampolineCallArgs<Args...> args_type;
     args_type()(function, args...);
   }
 };
